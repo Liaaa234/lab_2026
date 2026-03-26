975d491cf7a0fc1d6bf4452ef591cb42f1186ce4
Liaaa234 <giu.gt04@gmail.com>
2026-03-26 09:35:15 +0100

    add requirements

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
-app = FastAPI()   #tramite attraverso il quale interagiamo con l'applicazione (?)
+app = FastAPI()
+templates = Jinja2Templates(directory="./templates")   #dobbiamo dire al motore di templating dove sono contenuti i nostri file di templates
+#meccamismo che ci permettere di prendere il codice che abbiamo scritto e metterlo in output si chiama TEMPLATING
 
-@app.get("/")      #tra parentesi il percorso dell'end point (ora è sul percorso radice "/")
-def hello_world():              #funzione classica, per farla diventare un end point bisogna aggiungere un decoratore
-    return "Hello World!"   #per lanciare l'applicazione (?) si usa "fastapi dev" e scrivendo sul browser "localhost:8000" vediamo il risultato
+"""
+@app.get("/", response_class = HTMLResponse)    #formato in cui fastapi deve restituire il valore della funzione (?) -----STATICO
+def home(request: Request):
+    *Renders the home page.* COMMENTO
+    return templates.TemplateResponse(
+        request=request, name='home.html'   #quale file html passare al sito (in questo caso home o base)
+    )   #ricordarsi questo pattern perchè verrà ripetuto
+"""
+
+@app.get("/", response_class = HTMLResponse)    #PER POPOLARE IL SITO IN MODO DINAMICO
+def home (request: Request):
+    text = {
+        "title": "Home page",
+        "content": "Welcome to the home page"
+    }
+    context = {
+        "text": text,
+        "sequence": ["a", "b", "c"]
+    }
+    return templates.TemplateResponse(
+        request=request,
+        name="home.html",
+        context=context
+    )
