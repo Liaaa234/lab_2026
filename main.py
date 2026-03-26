756f547e0ecee8e15698f60f9ec5028136e5f711
Liaaa234 <giu.gt04@gmail.com>
2026-03-26 18:35:41 +0100

    add requirements

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-templates = Jinja2Templates(directory="./templates")   #dobbiamo dire al motore di templating dove sono contenuti i nostri file di templates
-#meccamismo che ci permettere di prendere il codice che abbiamo scritto e metterlo in output si chiama TEMPLATING
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates= Jinja2Templates(directory="templates")
 
-"""
-@app.get("/", response_class = HTMLResponse)    #formato in cui fastapi deve restituire il valore della funzione (?) -----STATICO
-def home(request: Request):
-    *Renders the home page.* COMMENTO
-    return templates.TemplateResponse(
-        request=request, name='home.html'   #quale file html passare al sito (in questo caso home o base)
-    )   #ricordarsi questo pattern perchè verrà ripetuto
-"""
-
-@app.get("/", response_class = HTMLResponse)    #PER POPOLARE IL SITO IN MODO DINAMICO
+product_list = [
+    {"name": "notebook DELL", "price": 2999.99, "location": "Cagliari"},
+    {"name": "notebook HP", "price": 1999.99, "location": "Sassari"},
+    {"name": "notebook ASUS", "price": 1499.99, "location": "Samatzai"},
+    {"name": "notebook ACER", "price": 999.99, "location": "Fanculo"},
+]
+@app.get("/", response_class=HTMLResponse)#per aggiungere questa funzione all'endpoint dobbiamo mettere un decoratore, inzia con @ poi la variabile a cui si riferisce e poi il .metodo e tra parentesi il percorso
 def home (request: Request):
-    text = {
-        "title": "Home page",
-        "content": "Welcome to the home page"
-    }
-    context = {
-        "text": text,
-        "sequence": ["a", "b", "c"]
-    }
+
     return templates.TemplateResponse(
         request=request,
-        name="home.html",
-        context=context
+        name='home.html',
+        context={"text": "Welcome to the store"}
+    )
+
+@app.get("/products", response_class=HTMLResponse)
+def products(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="products.html",
+        context={"product_list": product_list}
     )
