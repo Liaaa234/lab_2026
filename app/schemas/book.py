11a5098c5db4eea4cd4b30ed2a973d513f6fa47e
Liaaa234 <giu.gt04@gmail.com>
2026-04-29 10:06:52 +0200

    add requirements

--- app/schemas/book.py
+++ app/schemas/book.py
@@ -3,6 +3,9 @@
 from pydantic import BaseModel, Field
 from typing import Annotated
 
+class BookPath(BaseModel):
+    title: str | None = None
+    author: str | None = None
 
 class Book(BaseModel):
     id: int
@@ -27,6 +30,6 @@ class Book(BaseModel):
 
 books =  {
     0: Book(id=0, title="Il nome della Rosa", author="Umberto Eco", review=5),
-    1: Book(id=1, title="Il gioco dei sei", author="Umberto Eco", review=4),
+    1: Book(id=1, title="Il gioco dei sei", author="Umberto Eco", review=1),
     2: Book(id=0, title="Il gioco dei sette", author="Maccio Capatonda", review=3)
 }
