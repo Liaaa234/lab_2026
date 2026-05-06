2a16a9c31049d4dc97e7b19179bf046b65702a7c
Liaaa234 <giu.gt04@gmail.com>
2026-05-06 09:32:27 +0200

    add requirements

--- app/data/db.py
+++ app/data/db.py
@@ -1,4 +1,6 @@
 from sqlmodel import create_engine, SQLModel, Session
+from typing import Annotated
+from fastapi import Depends
 
 sqlite_file_name = "C:\\Users\\gika1\\lab_2026\\app\\data\\database.db"   #file persistente in memoria
 sqlite_url = f"sqlite:///{sqlite_file_name}"    #file endpoint dove viene montato qualcosa(?)
@@ -10,3 +12,10 @@ engine = create_engine(
 #inizializziamo il database
 def init_database():
     SQLModel.metadata.create_all(engine)
+
+#dependecies
+def get_session():
+    with Session(engine) as session:      #passare il motore del database che è stato inizializzato
+        yield session   #facendo yield la sessione rimane bloccata qui e ogni volta che chiamiamo la funzione riapre sempre la stessa istanza che è stata aperta in precedenza (?)
+
+SessionDep = Annotated[Session, Depends(get_session)]  #salvo la dependen dentro una variabile per non riscriverla ogni volta
